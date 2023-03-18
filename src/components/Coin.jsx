4fa3638503db39b58bf14253c92ff4e631fa9b6f
Usman077 <usmansheikh4949@gmail.com>
2023-03-18 23:15:28 +0500

    update

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react';
+import axios  from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Coin = () => {
-  return (
-    <div>
-      <h1>Coin</h1>
+    
+ const [exchanges,setexchanges] = useState([])
+ const [loading,setloading] = useState(true)
+ const server = "https://api.coingecko.com/api/v3";
+     useEffect(()=>{
+ const fatchExchange= async ()=>{
+    const {data} =await axios.get(`${server}/coins/markets?vs_currency=pkr`)
+    setexchanges(data) 
+    console.log(data)
+    setloading(false    )
+ 
+}
+  fatchExchange();
+     },[])
+     
+     
+     return (
+         <div  className="main">
+             <h1>Coin</h1>
+           <div className="container">
+
+             <div className='card'>
+    
+        {exchanges.map((i)=>(
+          <div className="Coincard" key={i.id}>
+             
+          
+        
+            <div className="card-img">
+                     <img src={i.image} alt="image" width="40" height="40" />
+                </div>
+           
+            <div className="card-symbol">
+                <h4>{i.symbol}</h4>
+                </div>
+            
+            <div className="card-price">
+                <h4>{" price:"+"pkr"+i.current_price}</h4>
+                </div>
+            
+           
+            </div>
+     
+      
+     ))}
+   <ClipLoader
+     color= "#000000"
+     loading={loading}
+    
+     size={150}
+     aria-label="Loading Spinner"
+     data-testid="loader"
+     />
+     
+     </div>
+    
+       <div>
+     
+       </div>
+
+    
+     </div>
     </div>
   )
 }
--- src/components/Exchange.jsx
+++ src/components/Exchange.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect,useState } from 'react';
 import axios  from "axios";
-import Loader from './loader';
+import ClipLoader from "react-spinners/ClipLoader";
 
 const Exchange = () => {
     
  const [exchanges,setexchanges] = useState([])
- const [Loader,setLoader] = useState(true)
+ const [loading,setloading] = useState(true)
  const server = "https://api.coingecko.com/api/v3";
      useEffect(()=>{
  const fatchExchange= async ()=>{
     const {data} =await axios.get(`${server}/exchanges`)
     setexchanges(data) 
+    setloading(false    )
  
 }
   fatchExchange();
@@ -18,29 +19,49 @@ const Exchange = () => {
 
 
      return (
-         <div  className="container">
-     
+         <div  className="main">
              <h1>Exhange</h1>
-             
+           <div className="container">
+
+             <div className='card'>
     
         {exchanges.map((i)=>(
             <div className="Coincard" key={i.id}>
-              <a href={i.url}>
-                <img src={i.image} alt="image"  width="50px" height="50px"/>
-                <p>{i.name}</p>
-                <p>{i.rank}</p>
+              <a href={i.url} textdecoration="none">
+            
+            <span className="card-img">
+                     <img src={i.image} alt="image" width="40" height="40" />
+                </span>
+            <span className="card-rank">
+                <h3>{i.trust_score_rank}</h3>
+                </span>
+                
+            <span className="card-name">
+                <h4>{i.name}</h4>
+                </span>
               </a>
             </div>
      
-              
-        ))}
-        
+     
+     ))}
+  
+   <ClipLoader
+     color= "#000000"
+     loading={loading}
+    
+     size={150}
+     aria-label="Loading Spinner"
+     data-testid="loader"
+     />
+     
+     </div>
     
        <div>
      
        </div>
 
     
+     </div>
     </div>
   )
 }
